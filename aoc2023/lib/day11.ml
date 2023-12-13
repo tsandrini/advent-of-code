6c83fb4429f13602c96d09da49091b6ab2fcc913
tsandrini <tomas.sandrini@seznam.cz>
2023-12-13 11:52:25 +0100

    feat(day11): reformat & add tests

--- aoc2023/lib/day11.ml
+++ aoc2023/lib/day11.ml
@@ -10,40 +10,110 @@ let parse input =
     let height = List.length mat in
     let width = List.length (List.hd_exn mat) in
     Array.init width ~f:(fun y ->
-        Array.init height ~f:(fun x ->
-            List.nth_exn (List.nth_exn mat x) y)) |> Array.map ~f:Array.to_list |> Array.to_list in
+        Array.init height ~f:(fun x -> List.nth_exn (List.nth_exn mat x) y))
+    |> Array.map ~f:Array.to_list |> Array.to_list
+  in
   let space =
     String.split_lines input
     |> List.map ~f:(fun line ->
-           String.to_list line
-           |> List.map ~f:int_node_type_of_char) in
-  let empty_rows = List.mapi space ~f:(fun i row -> if (List.fold_left ~init:0 ~f:(+) row) = 0 then i else -1) |> List.filter ~f:(fun y -> y <> -1) in
-  let empty_cols = transpose space |> List.mapi ~f:(fun i row -> if (List.fold_left ~init:0 ~f:(+) row) = 0 then i else -1) |> List.filter ~f:(fun x -> x <> -1) in
-  Printf.printf "Empty_rows = %s\n" (List.to_string empty_rows ~f:Int.to_string);
-  Printf.printf "Empty_cols = %s\n" (List.to_string empty_cols ~f:Int.to_string);
+           String.to_list line |> List.map ~f:int_node_type_of_char)
+  in
+  let empty_rows =
+    List.mapi space ~f:(fun i row ->
+        if List.fold_left ~init:0 ~f:( + ) row = 0 then i else -1)
+    |> List.filter ~f:(fun y -> y <> -1)
+  in
+  let empty_cols =
+    transpose space
+    |> List.mapi ~f:(fun i row ->
+           if List.fold_left ~init:0 ~f:( + ) row = 0 then i else -1)
+    |> List.filter ~f:(fun x -> x <> -1)
+  in
   let galaxies = ref [] in
-  List.iteri ~f:(fun y row ->
-      List.iteri ~f:(fun x node ->
-          if node = 1 then galaxies := (x, y) :: !galaxies;) row) space;
+  List.iteri
+    ~f:(fun y row ->
+      List.iteri
+        ~f:(fun x node -> if node = 1 then galaxies := (x, y) :: !galaxies)
+        row)
+    space;
   (space, !galaxies, (empty_rows, empty_cols))
 
 let manhattan_dist (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-let compute_distances ?(expansion_rate=2) ~galaxies ~empty:(empty_rows, empty_cols) =
+let compute_distances ?(expansion_rate = 2) ~galaxies
+    ~empty:(empty_rows, empty_cols) =
   let expansion_shift_in (x1, y1) (x2, y2) =
     let xmin, xmax = (min x1 x2, max x1 x2) in
     let ymin, ymax = (min y1 y2, max y1 y2) in
-    let cols_in = List.filter empty_cols ~f:(fun x -> x > xmin && x < xmax) in
-    let rows_in = List.filter empty_rows ~f:(fun y -> y > ymin && y < ymax) in
-    (* Printf.printf "Checking galaxies (%d, %d) and (%d, %d) - cols_in: %d, rows_in: %d\n" x1 y1 x2 y2 (List.length cols_in) (List.length rows_in); *)
-    expansion_rate * ((List.length cols_in) + (List.length rows_in)) in
+    let cols_in =
+      List.filter empty_cols ~f:(fun x -> x > xmin && x < xmax) |> List.length
+    in
+    let rows_in =
+      List.filter empty_rows ~f:(fun y -> y > ymin && y < ymax) |> List.length
+    in
+    expansion_rate * (cols_in + rows_in)
+  in
   let rec aux acc = function
     | [] -> acc
-    | hd :: tl -> aux (List.fold_left ~init:acc ~f:(fun acc' other -> acc' + (manhattan_dist hd other) + (expansion_shift_in hd other)) tl) tl in
+    | hd :: tl ->
+        aux
+          (List.fold_left ~init:acc
+             ~f:(fun acc' other ->
+               acc' + manhattan_dist hd other + expansion_shift_in hd other)
+             tl)
+          tl
+  in
   aux 0 galaxies
 
 let solve (_, galaxies, empty) =
-  Printf.printf "Part1 - sum of distances (expansion_rate=2): %d\n" (compute_distances ~galaxies ~empty ~expansion_rate:1);
-  Printf.printf "Part2 - sum of distances (expansion_rate=1000000): %d\n" (compute_distances ~galaxies ~empty ~expansion_rate:999999)
+  Printf.printf "Part1 - sum of distances (expansion_rate=2): %d\n"
+    (compute_distances ~galaxies ~empty ~expansion_rate:1);
+  Printf.printf "Part2 - sum of distances (expansion_rate=1000000): %d\n"
+    (compute_distances ~galaxies ~empty ~expansion_rate:999999)
 
 let main input = In_channel.read_all input |> parse |> solve
+
+let%test "Day11 part1 - example input" =
+  "...#......\n\
+   .......#..\n\
+   #.........\n\
+   ..........\n\
+   ......#...\n\
+   .#........\n\
+   .........#\n\
+   ..........\n\
+   .......#..\n\
+   #...#....." |> parse
+  |> (fun (_, galaxies, empty) ->
+       compute_distances ~galaxies ~empty ~expansion_rate:1)
+  = 374
+
+let%test "Day11 part2 - example input1" =
+  "...#......\n\
+   .......#..\n\
+   #.........\n\
+   ..........\n\
+   ......#...\n\
+   .#........\n\
+   .........#\n\
+   ..........\n\
+   .......#..\n\
+   #...#....." |> parse
+  |> (fun (_, galaxies, empty) ->
+       compute_distances ~galaxies ~empty ~expansion_rate:9)
+  = 1030
+
+let%test "Day11 part2 - example input2" =
+  "...#......\n\
+   .......#..\n\
+   #.........\n\
+   ..........\n\
+   ......#...\n\
+   .#........\n\
+   .........#\n\
+   ..........\n\
+   .......#..\n\
+   #...#....." |> parse
+  |> (fun (_, galaxies, empty) ->
+       compute_distances ~galaxies ~empty ~expansion_rate:99)
+  = 8410
