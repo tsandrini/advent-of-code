e2ccdf67cb7513d5dfda440ba95e7ecf153b2675
tsandrini <tomas.sandrini@seznam.cz>
2024-12-01 16:05:22 +0100

    feat(2024): add day01 solution

--- aoc/2024/src/bin/01.rs
+++ aoc/2024/src/bin/01.rs
@@ -1,11 +1,43 @@
 advent_of_code::solution!(1);
 
+use itertools::Itertools;
+use std::collections::HashMap;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines = parse(input);
+
+    Some(
+        lines
+            .iter()
+            .map(|(a, _)| a)
+            .sorted()
+            .zip(lines.iter().map(|(_, b)| b).sorted())
+            .map(|(a, b)| if a > b { a - b } else { b - a })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = parse(input);
+    let fst = lines.iter().map(|(a, _)| a);
+    let snd = lines.iter().map(|(_, b)| b);
+
+    let freq = snd.fold(HashMap::new(), |mut map, val| {
+        map.entry(val).and_modify(|f| *f += 1).or_insert(1);
+        map
+    });
+
+    Some(fst.map(|a| a * (freq.get(a).unwrap_or(&0))).sum())
+}
+
+pub fn parse(input: &str) -> Vec<(u32, u32)> {
+    input
+        .lines()
+        .map(|line| {
+            let split: Vec<_> = line.split_whitespace().collect();
+            (split[0].parse().unwrap(), split[1].parse().unwrap())
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -15,12 +47,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
